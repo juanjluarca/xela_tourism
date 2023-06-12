f66c3cbdc47a5e587104af37c7dfe95fa5d774bf
Diego-gr-23 <124418381+Diego-gr-23@users.noreply.github.com>
2023-06-11 22:53:47 -0600

    Parte principal de la página
    añadí un logo al principio, también añadí la imagen principal de la página y le di estilo a esa imagen, después  añadí una imagen de Quetzaltenango y información sobre Quetzaltenango, después le di estilo a esa imagen y a la información

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter, Navigate,Route,Routes } from "react-router-dom";
+import Principal from './page/Principal';
+
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path ="/principal" element={<Principal/>} />
       <Route path="*" element={<Navigate to="/" replace={true} /> } exact={true} />
     </Routes>
     </BrowserRouter>
   );
-}
-
+  }
 export default App;
